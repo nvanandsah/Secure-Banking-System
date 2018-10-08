37914ad30fa7054897cc414c1ae054ff084b4ce8
nvanandsah <navneet16170@iiitd.ac.in>
2018-10-08 16:23:15 +0530

    Changes

--- transaction/models.py
+++ transaction/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+class transaction():
+        full_name = models.CharField(
+        max_length=256,
+        blank=False,
+        validators=[
+                RegexValidator(
+                    regex=REGEX,
+                    message='Name must be Alphabetic',
+                    code='invalid_full_name'
+                    )
+                ]
+        )
+        acc_no = models.PositiveIntegerField(
+        unique=True,
+        validators=[
+            MinValueValidator(10000000),
+            MaxValueValidator(99999999)
+            ]
+        )
