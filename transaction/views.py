482cb2539f6fc48b593421fa3222279ea7490808
nvanandsah <navneet16170@iiitd.ac.in>
2018-10-15 08:31:23 +0530

    changes

--- transaction/views.py
+++ transaction/views.py
@@ -5,7 +5,7 @@ def transaction(request):
     if not (request.user.is_authenticated):
         return render(request,"base/home.html",{})
     else:
-        return render(request,"transaction/tr_page.html",{'name' : request.user.name,'Acc':request.user.acc_no,'bal':request.user.balance})
+        return render(request,"transaction/tr_page.html",{'name' : request.user.full_name,'Acc':request.user.acc_no,'bal':request.user.balance})
 
 def trnsac(request):
     if not request.user.is_authenticated:
@@ -27,5 +27,5 @@ def trnsac(request):
         context = {"form": form,
                    "title": title
                    }
-        return render(request, "login/form.html", context)
+        return render(request, "transaction/tr_page.html", context)
 
