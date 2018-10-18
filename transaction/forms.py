1b47abb3cb7f02de5da4cd97eabbe54774b43c90
nvanandsah <navneet16170@iiitd.ac.in>
2018-10-18 06:41:32 +0530

    add money remove model, working

--- transaction/forms.py
+++ transaction/forms.py
@@ -1,7 +1,6 @@
 from django import forms
 from transaction.models import TX_in
 
-from .models import addmoney_own
 class trnsction(forms.ModelForm):
     class Meta:
         model = TX_in
@@ -22,7 +21,7 @@ class trnsction(forms.ModelForm):
             raise forms.ValidationError('You have to write something!')
 
 
-class addMoney(forms.ModelForm):
+'''class addMoney(forms.ModelForm):
     class Meta:
         model = addmoney_own
         fields = {
@@ -40,19 +39,17 @@ class addMoney(forms.ModelForm):
         if  not Account_No and not message and not Amount:
             raise forms.ValidationError('You have to write something!')
 '''             
-class trnsction(forms.Form):
-    name = forms.CharField(max_length=30)
+class addMoney(forms.Form):
+    Amount = forms.IntegerField(label="Amount")
     acc_no = forms.IntegerField(label="acc_no")
     message = forms.CharField(
         max_length=2000,
         widget=forms.Textarea(),
         help_text='Write here your message!'
     )
-    amount = forms.IntegerField(label="Amount")
-
     def clean(self):
-        name = self.cleaned_data.get('full_name')
+        amount = self.cleaned_data.get('Amount')
         message = self.cleaned_data.get('message')
         acc_no=self.cleaned_data.get("acc_no")
-        if not name and not email and not message:
-            raise forms.ValidationError('You have to write something!')'''
+        if not amount and not acc_no:
+            raise forms.ValidationError('You have to write something!')
