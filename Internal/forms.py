40165c0cce05e0af8bdf1c9c7a930932eb046649
nvanandsah <navneet16170@iiitd.ac.in>
2018-10-20 07:49:15 +0530

    Deleted internal model

--- Internal/forms.py
+++ Internal/forms.py
@@ -2,7 +2,7 @@ import datetime
 from django import forms
 from django.contrib.auth import authenticate
 from django.contrib.auth.forms import UserCreationForm
-from .models import User
+from login.models import User
 
 class UserRegistrationForm(UserCreationForm):
 
